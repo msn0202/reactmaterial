1bd28d435536a0235f967747060716bfe699b475
msn0202 <76164264+msn0202@users.noreply.github.com>
2024-11-11 13:23:03 +0530

    new code added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import ResponsiveGrid from './ResponsiveGrid';
 
 function App() {
   return (
     <div className="App">
    <h1>REACT MATERIAL</h1>
+   <ResponsiveGrid/>
     </div>
   );
 }
