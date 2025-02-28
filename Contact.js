e2648b7da48ef0fa8cda9fb3835734becf22bdf3
msn0202 <76164264+msn0202@users.noreply.github.com>
2025-02-28 12:09:51 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Contact(props) {
+  return (
+    <>
+      <div className="header-bar">
+        <Navbar />
+      </div>
+      <div>
+        <h3>HELLO CONTACT</h3>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
